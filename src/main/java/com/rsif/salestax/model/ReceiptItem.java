1f7b6ad21f78c669d8b097d4d986e882932c0970
Mazy Toufik <mazytoufik@outlook.com>
2023-10-24 13:06:37 +0200

    fix config SalesTaxControllerTest

--- src/main/java/com/rsif/salestax/model/ReceiptItem.java
+++ src/main/java/com/rsif/salestax/model/ReceiptItem.java
@@ -4,12 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.math.BigDecimal;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReceiptItem {
     private String name;
-    private BigDecimal total;
+    private double total;
 }
